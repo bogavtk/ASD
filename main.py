832bb62a5a565b16387c4c56dcd8caebe39726e7
bogdan <bogdan.vtk.4516@gmail.com>
2022-06-03 00:02:35 +0300

    ASD - 4 Homework

--- main.py
+++ main.py
@@ -1,77 +1,47 @@
-class Node:
-    value: object = None
+def mergeSort(myList):
+    if len(myList) > 1:
+        mid = len(myList) // 2
+        left = myList[:mid]
+        right = myList[mid:]
 
-    def __repr__(self):
-        return str(self.value)
+        # Рекурсивный вывод
+        mergeSort(left)
+        mergeSort(right)
+
+        # Итераторы - половинки двух списков
+        i = 0
+        j = 0
+
+        # Итератор для конечного списка
+        k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+
+                # Было использовано значение из левой половины
+                myList[k] = left[i]
+
+                # Переместить итератор вперед
+                i += 1
+            else:
+                myList[k] = right[j]
+                j += 1
+
+            # Перейти к следующему слоту
+            k += 1
+
+        # Для всех остальных значений
+        while i < len(left):
+            myList[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            myList[k] = right[j]
+            j += 1
+            k += 1
 
 
-
-class LinkedList:
-    def __init__(self):
-        self._length: int = 0
-        self.head: "Node" = None
-
-    def length(self) -> int:
-        return self._length
-
-    def __len__(self):
-        return self.length()
-
-
-class PriorNode(Node):
-    nnext: "Node"
-
-    def __init__(self, value, nnext=None, priority=None):
-        self.value = value
-        self.nnext = nnext
-        self.priority = priority
-
-
-class PriorQueue(LinkedList):
-    def __init__(self):
-        super().__init__()
-
-    def enqueue(self, value, priority):
-        self._length += 1
-
-        if not self.head:
-            self.head = PriorNode(value, None, priority)
-            return
-
-        if self.head.priority > priority:
-            old_head = self.head
-            self.head = PriorNode(value, None, priority)
-            self.head.nnext = old_head
-
-        else:
-            node_pointer = self.head
-            while node_pointer.nnext and node_pointer.priority <= priority:
-                node_pointer = node_pointer.nnext
-            old_pointer = node_pointer.nnext
-            node_pointer.nnext = PriorNode(value, None, priority)
-            node_pointer.nnext.nnext = old_pointer
-
-    def dequeue(self) -> object:
-        node_pointer = self.head
-        if len(self) == 0:
-            raise IndexError('Список пуст!')
-        elif len(self) == 1:
-            self._length -= 1
-            self.head = None
-            return
-        elif len(self) == 2:
-            self._length -= 1
-            self.head.nnext = None
-            return
-        else:
-            for i in range(1, len(self)):
-                self._length -= 1
-                if i == len(self)-1:
-                    node_pointer.nnext = None
-                node_pointer = node_pointer.nnext
-
-    def check(self) -> object:
-        node_pointer = self.head
-        while node_pointer.nnext:
-            node_pointer = node_pointer.nnext
-        return node_pointer.value
+myList = [1, 3, 44, 12, 4, 5, 2]
+mergeSort(myList)
+print(myList)
